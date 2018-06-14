f8abd38f98738720dfb446ac6ad8b0844dfbafcd
Viktor Zhurbin <vzhurbin@gmail.com>
2018-06-14 08:10:04 +0300

    add Dashboard component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 import { handleInitialData } from '../actions/shared'
+import Dashboard from "./Dashboard";
 
 class App extends Component {
   componentDidMount() {
@@ -11,10 +12,18 @@ class App extends Component {
   render() {
     return (
       <div>
-        Starter Code.
+        {this.props.loading === true
+          ? null
+          : <Dashboard />}
       </div>
     )
   }
 }
 
-export default connect()(App)
+function mapStateToProps({ authedUser }) {
+  return {
+    loading: authedUser === null
+  }
+}
+
+export default connect(mapStateToProps)(App)
